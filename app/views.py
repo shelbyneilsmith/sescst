2ccfd5506c3659dbad15b7e78b22815f01c5cdb0
Shelby Neil Smith <shelbyneilsmith@gmail.com>
2016-04-14 15:42:33 -0500

    setup postgresql database stuff

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 # from flask.ext.login import login_user, logout_user, current_user, login_required
-from app import app
+from app import app, db
 
 # main index page view
 @app.route('/')
