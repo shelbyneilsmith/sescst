6fe2ee10b39a3ca502ef69cb3d0053e3047c9cdd
Shelby Neil Smith <shelbyneilsmith@gmail.com>
2016-07-29 16:06:46 -0500

    Troubleshooting 14

--- app/views/admin.py
+++ app/views/admin.py
@@ -9,7 +9,7 @@ from sqlalchemy.exc import IntegrityError
 import json
 
 from .. import db, login_required
-# from ..model import AppSettings, User, District, School, Role, Activity_Type, Activity_Log, Expense_Sheet, School_Type, School_Level, Location_Service
+from ..model import AppSettings, User, District, School, Role, Activity_Type, Activity_Log, Expense_Sheet, School_Type, School_Level, Location_Service
 # from ..schemas import AppSettingsSchema, UserSchema, RoleSchema, DistrictSchema, SchoolSchema, Activity_TypeSchema, Activity_LogSchema, Expense_SheetSchema, School_TypeSchema, School_LevelSchema, Location_ServiceSchema
 from ..forms.admin import RegisterUserForm, RegisterDistrictForm, RegisterSchoolForm
 from ..util.security import ts
