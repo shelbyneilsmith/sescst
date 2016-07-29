67be42dfa8d47e5248cb7e953fe269bea56f621e
Shelby Neil Smith <shelbyneilsmith@gmail.com>
2016-07-29 11:18:51 -0500

    Change home screen temporary text. Couple other bugs

--- app/static/source/js/components/utils.js
+++ app/static/source/js/components/utils.js
@@ -1,11 +1,5 @@
 'use strict';
 
 module.exports = angular.module('sescst-utils', [])
-	// .run(['$rootScope', function($rootScope) {
-	// 	// $rootScope.Utils = {
-	// 	// 	keys: Object.keys
-	// 	// };
-	// 	$rootScope.constructor.prototype.getKeys = Object.keys;
-	// }])
 	.directive('dynamicElement', require('../directive/utils/dynamicElement'))
 	.directive('staticInclude', require('../directive/utils/staticInclude'));
