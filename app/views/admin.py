b91ee2097f41d00549c42d6b4a9194a3a2ab5c18
Shelby Neil Smith <shelbyneilsmith@gmail.com>
2016-08-01 14:53:03 -0500

    Troubleshooting ??5

--- app/views/admin.py
+++ app/views/admin.py
@@ -10,7 +10,7 @@ import json
 
 from .. import db, login_required
 from ..model import AppSettings, User, District, School, Role, Activity_Type, Activity_Log, Expense_Sheet, School_Type, School_Level, Location_Service
-from ..schemas import AppSettingsSchema
+from ..schemas import AppSettingsSchema, UserSchema, RoleSchema
 # from ..schemas import AppSettingsSchema, UserSchema, RoleSchema, DistrictSchema, SchoolSchema, Activity_TypeSchema
 # from ..schemas import Activity_LogSchema, Expense_SheetSchema, School_TypeSchema, School_LevelSchema, Location_ServiceSchema
 from ..forms.admin import RegisterUserForm, RegisterDistrictForm, RegisterSchoolForm
